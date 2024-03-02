b7cda2cacb664e2e6929af7760735a081374b1bd
Abdullah KAYA <abdullahky0409@gmail.com>
2024-03-02 15:42:50 +0300

    descriptions are added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean primeNumberCheck (int number, int divisor ) {
+    public static boolean primeNumberCheck (int number, int divisor ) {  // Checking number is prime or not.
         if (number <= 1 ) {
             return false;
         }
@@ -17,11 +17,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        System.out.print("Please enter number = ");
+        System.out.print("Please enter number = ");    // Get values from user.
         int number= input.nextInt();
-        int divisor= number-1;
+        int divisor= number-1;                         // The definition is made for the divisor to divide the number up to one less than itself.
 
-        if(primeNumberCheck(number, divisor)){
+        if(primeNumberCheck(number, divisor)){         // Result is printed
             System.out.println(number + " is prime number") ;
         } else{
             System.out.println(number + " is not prime number");}
